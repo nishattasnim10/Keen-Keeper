de54ccbf2c981000f015f66fa79aa6e5ed5af462
nishattasnim10 <nishat.suchi10@gmail.com>
2026-04-16 22:15:33 +0600

    your friends section updated

--- src/components/homepage/YourFriends.jsx
+++ src/components/homepage/YourFriends.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import FriendsCard from '../ui/FriendsCard';
 
 const YourFriends = () => {
     const [friends, setFriends]= useState([]);
@@ -21,44 +22,11 @@ const YourFriends = () => {
 
             {/* Grid Container - Responsible for the 4-column layout */}
             <div className='w-9/12 md:w-9/12 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
-                {friends.map((friend, ind) => (
-                    <div key={ind} className="card bg-white shadow-sm border border-gray-100 rounded-2xl hover:shadow-md transition-shadow">
-                        <figure className="px-10 pt-10">
-                            <img
-                                src={friend.picture}
-                                alt={friend.name}
-                                className="w-24 h-24 rounded-full object-cover border-4 border-white shadow-sm"
-                            />
-                        </figure>
-
-                        <div className="card-body items-center text-center p-4">
-                            <h2 className="text-xl font-bold text-gray-800">{friend.name}</h2>
-                            <p className="text-gray-400 text-sm ">{friend.days_since_contact}</p>
-
-                            {/*tags logic*/}
-                            <div className="flex flex-wrap gap-2 justify-center mb-4">
-                                {friend.tags?.map((tag, i) => (
-                                    <span key={i} className="bg-[#DCFCE7] text-[#166534] text-[10px] font-bold px-3 py-1 rounded-full uppercase">
-                                        {tag}
-                                    </span>
-                                ))}
-                            </div>
-
-                            {/*status badge logic*/}
-                            <div className="card-actions">
-                                <span className={`px-5 py-1.5 rounded-full text-xs font-semibold text-white ${friend.status==="Overdue" 
-                                ? "bg-[#EF4444]" 
-
-                                : friend.status==="Almost Due"?"bg-[#F59E0B]" 
-
-                                : "bg-[#064E3B]"
-                                    }`}>
-                                    {friend.status}
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                {friends.map((friend, ind) => {
+                    return(
+                    <FriendsCard friend={friend} key={ind} />
+                    );
+      })};
             </div>
         </div>
     );
---
+++ src/components/ui/FriendsCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const FriendsCard = ({friend}) => {
+  return (
+      <div  className="card bg-white shadow-sm border border-gray-100 rounded-2xl hover:shadow-md transition-shadow">
+          <figure className="px-10 pt-10">
+              <img
+                  src={friend.picture}
+                  alt={friend.name}
+                  className="w-24 h-24 rounded-full object-cover border-4 border-white shadow-sm"
+              />
+          </figure>
+
+          <div className="card-body items-center text-center p-4">
+              <h2 className="text-xl font-bold text-gray-800">{friend.name}</h2>
+              <p className="text-gray-400 text-sm ">{friend.days_since_contact}</p>
+
+              {/*tags logic*/}
+              <div className="flex flex-wrap gap-2 justify-center mb-4">
+                  {friend.tags?.map((tag, i) => (
+                      <span key={i} className="bg-[#DCFCE7] text-[#166534] text-[10px] font-bold px-3 py-1 rounded-full uppercase">
+                          {tag}
+                      </span>
+                  ))}
+              </div>
+
+              {/*status badge logic*/}
+              <div className="card-actions">
+                  <span className={`px-5 py-1.5 rounded-full text-xs font-semibold text-white ${friend.status === "Overdue"
+                      ? "bg-[#EF4444]"
+
+                      : friend.status === "Almost Due" ? "bg-[#F59E0B]"
+
+                          : "bg-[#064E3B]"
+                      }`}>
+                      {friend.status}
+                  </span>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default FriendsCard
